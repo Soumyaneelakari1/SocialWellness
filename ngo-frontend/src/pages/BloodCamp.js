df5334608f69e9ac97dcb51460499ab8cfaa864e
Manasa54321 <105267382+Manasa54321@users.noreply.github.com>
2024-06-09 12:11:23 +0530

    Update BloodCamp.js

--- ngo-frontend/src/pages/BloodCamp.js
+++ ngo-frontend/src/pages/BloodCamp.js
@@ -1,20 +1,30 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { Link, useParams } from 'react-router-dom';
 import axios from "axios";
+import AuthContext from '../AuthContext';
 
 export default function BloodCamp() {
   const[camps,setCamps] = useState([]);
-
+  const [userRole, setUserRole] = useState('');
   const { id } = useParams();
+  const { authState } = useContext(AuthContext);
 
   useEffect(() => {
     loadCamps();
+    fetchUserRole(authState.userId);
   },[]);
 
   const loadCamps = async () =>{
     const res = await axios.get("http://localhost:8080/bloodDonation");
     setCamps(res.data);
   };
+
+  const fetchUserRole = async (userId) => {
+    const res = await axios.get(`http://localhost:8080/user/role?userId=${userId}`);
+    setUserRole(res.data);
+    console.log(userRole)
+  };
+
 
   const deleteCamp = async(id) => {
     await axios.delete(`http://localhost:8080/bloodDonation/${id}`);
@@ -44,32 +54,28 @@ export default function BloodCamp() {
                   <td>{camp.day}</td>
                   <td>{camp.venue}</td>
                   <td>
-                    {/* <Link 
-                    className='btn btn-primary mx-2'
-                    to={`/viewCamp/${camp.id}`}
-                    >
-                      View
-                    </Link> */}
-                    <Link
-                    className="btn btn-outline-primary mx-2"
-                    to={`/editCamp/${camp.id}`}
-                  >
-                    Edit
-                  </Link>
-                  <button
-                    className="btn btn-danger mx-2"
-                    onClick={() => deleteCamp(camp.id)}
-                  >
-                    Delete
-                  </button>
+                  {userRole === 'admin' && (
+                    <>
+                      <Link className="btn btn-outline-primary mx-2" to={`/editCamp/${camp.id}`}>
+                        Edit
+                      </Link>
+                      <button className="btn btn-danger mx-2" onClick={() => deleteCamp(camp.id)}>
+                        Delete
+                      </button>
+                    </>
+                  )}
                   </td>
                 </tr>
                ))}
             </tbody>
           </table>
         </div>
-        <Link className='btn btn-primary' to='/addcamp'>Add Blood Donation Camp</Link>
-      </div>
+        {userRole === 'admin' && (
+        <Link className="btn btn-primary" to="/addcamp">
+          Add Blood Donation Camp
+        </Link>
+      )}
+    </div>
         
     )
 }
